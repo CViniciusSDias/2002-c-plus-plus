b5b4523c6a08bd04c5d7188fc0794d9e8a62b007
Vinicius Dias <carlosv775@gmail.com>
2020-11-14 19:25:48 -0300

    Vídeo 5.3

--- Forca/Forca/imprime_erros.cpp
+++ Forca/Forca/imprime_erros.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include "imprime_erros.hpp"
 
-namespace Forca {
-    void imprime_erros(const std::vector<char>& chutes_errados)
-    {
-        std::cout << "Chutes errados: ";
-        for (char letra: chutes_errados) {
-            std::cout << letra << " ";
-        }
-        std::cout << std::endl;
+void Forca::imprime_erros(const std::vector<char>& chutes_errados)
+{
+    std::cout << "Chutes errados: ";
+    for (char letra: chutes_errados) {
+        std::cout << letra << " ";
     }
+    std::cout << std::endl;
 }
